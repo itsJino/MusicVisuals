b183cbfa45e0f46085700855136a986c2db507bd
Joshua <floresjoshua2603@gmail.com>
2023-05-01 13:44:22 +0100

    Setup Main Visual

---
+++ java/src/ie/tudublin/MainVisual.java
@@ -0,0 +1,24 @@
+package ie.tudublin;
+
+import ddf.minim.*;
+import C21352183.*;
+import C21436494.*;
+import C21755919.*;
+
+public class MainVisual extends Visual {
+    public void settings() {
+        size(1920, 1080, P3D);
+    }
+
+    public void setup() {
+
+        colorMode(RGB);
+    }
+
+    public void draw() {
+        background(0);
+        translate(width / 2, height / 2);
+
+        
+    }
+}
